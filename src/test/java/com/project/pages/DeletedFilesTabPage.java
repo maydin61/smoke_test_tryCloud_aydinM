a69c5c6d9f3ec446d23be1e47aaa44e05099abab
Kozma Akos <kozmaakoss@gmail.com>
2023-04-04 22:01:57 +0300

    first AC is done

--- src/test/java/com/project/pages/DeletedFilesTabPage.java
+++ src/test/java/com/project/pages/DeletedFilesTabPage.java
@@ -1,4 +1,25 @@
 package com.project.pages;
 
+import com.project.utilites.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class DeletedFilesTabPage {
+
+    public DeletedFilesTabPage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+    @FindBy(xpath = "//a[@href='/index.php/apps/files/']")
+    public WebElement filesButton;
+
+    @FindBy(xpath = "//a[.='Deleted files']")
+    public WebElement deletedFiles;
+
+    @FindBy(xpath = "//a[.='Deleted']")
+    public WebElement deletedButton;
+
+    @FindBy(xpath = "//span[@class='nametext extra-data']")
+    public WebElement recentDeletedFile;
 }
