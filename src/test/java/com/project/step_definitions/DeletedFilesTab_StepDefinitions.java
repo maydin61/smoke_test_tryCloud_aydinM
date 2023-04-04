09b195792f5b1839e729e2c808f3a6e2ee610709
Kozma Akos <kozmaakoss@gmail.com>
2023-04-04 15:50:20 +0300

    created StepDefinitions class

---
+++ src/test/java/com/project/step_definitions/DeletedFilesTab_StepDefinitions.java
@@ -0,0 +1,33 @@
+package com.project.step_definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class DeletedFilesTab_StepDefinitions {
+
+
+
+    @Given("User is on the dashboard page")
+    public void user_is_on_the_dashboard_page() {
+
+    }
+
+    @When("user clicks on -Files- button")
+    public void user_clicks_on_files_button() {
+
+    }
+    @When("user clicks on -Deleted files- button")
+    public void user_clicks_on_deleted_files_button() {
+
+    }
+    @When("user clicks on -Deleted- button")
+    public void user_clicks_on_deleted_button() {
+
+    }
+    @Then("user should see the most recent deleted file in the first line")
+    public void user_should_see_the_most_recent_deleted_file_in_the_first_line() {
+
+    }
+
+}
