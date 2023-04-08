0ab6c42783a22108291f63e12d93fcca9b3dc32c
Kozma Akos <kozmaakoss@gmail.com>
2023-04-08 11:18:07 +0300

    dynamic find recent time

--- src/test/java/com/project/step_definitions/DeletedFilesTab_StepDefinitions.java
+++ src/test/java/com/project/step_definitions/DeletedFilesTab_StepDefinitions.java
@@ -1,7 +1,6 @@
 package com.project.step_definitions;
 
 import com.project.pages.DeletedFilesTabPage;
-import com.project.pages.LoginPage;
 import com.project.utilites.BrowserUtils;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
@@ -10,7 +9,6 @@ import org.junit.Assert;
 
 public class DeletedFilesTab_StepDefinitions {
 
-    LoginPage loginPage = new LoginPage();
     DeletedFilesTabPage deletedFilesTabPage = new DeletedFilesTabPage();
 
     @When("user clicks on -Files- button")
@@ -20,7 +18,9 @@ public class DeletedFilesTab_StepDefinitions {
     @When("user clicks on -Deleted files- button")
     public void user_clicks_on_deleted_files_button() {
         deletedFilesTabPage.deletedFiles.click();
+        DeletedFilesTabPage.expected=DeletedFilesTabPage.closestTime();
     }
+
     @When("user clicks on -Deleted- button")
     public void user_clicks_on_deleted_button() {
         BrowserUtils.waitForVisibility(deletedFilesTabPage.deletedButton,10);
@@ -28,7 +28,7 @@ public class DeletedFilesTab_StepDefinitions {
     }
     @Then("user should see the most recent deleted file in the first line")
     public void user_should_see_the_most_recent_deleted_file_in_the_first_line() {
-        Assert.assertEquals("Readme\n.md",deletedFilesTabPage.recentDeletedFile.getText());
+        Assert.assertEquals(DeletedFilesTabPage.expected,deletedFilesTabPage.recentDeletedFileTime.getAttribute("data-original-title"));
     }
 
     @And("user clicks on -Name- button")
