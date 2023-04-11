0e3152a3a4a97c6436b60217ea69318bcc92abe3
docbaylan <112724476+docbaylan@users.noreply.github.com>
2023-04-11 21:33:05 +0200

    Widget rewised and tested.

--- src/test/java/com/project/runners/CukesRunner.java
+++ src/test/java/com/project/runners/CukesRunner.java
@@ -18,7 +18,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/project/step_definitions",
         dryRun = false,
-        tags = "@wip"
+        tags = ""
 )
 
 
