ccd5dc6a288819571b0b53b1253d100ba644ba3c
Hüseyin Yıldırım <11huseyinyildirim@gmail.com>
2023-04-12 22:48:20 +0200

    New web element added.

--- src/test/java/com/project/pages/CalendarModule.java
+++ src/test/java/com/project/pages/CalendarModule.java
@@ -1,14 +1,14 @@
 package com.project.pages;
 
+import com.project.step_definitions.Calendar_StepDefs;
 import com.project.utilites.Driver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-public class CalendarModule {
+public class CalendarModule{
 
     public CalendarModule(){
-
         PageFactory.initElements(Driver.getDriver(),this);
     }
 
@@ -24,6 +24,29 @@ public class CalendarModule {
 
     @FindBy(xpath = "//span[.='Month']")
     public WebElement monthlyCalendarViewIcon;
+
+    @FindBy(xpath = "//button[@class='button primary new-event']")
+    public WebElement newEventButton;
+
+    @FindBy(xpath = "//input[@placeholder='Event title']")
+    public WebElement eventTitleInputBox;
+
+    @FindBy(xpath = "//button[@class='primary']")
+    public WebElement saveButton;
+
+    @FindBy(xpath = "//*[contains(text(),'"+Calendar_StepDefs.eventTitleForDeleteFeature+"')]")
+    public WebElement newCreatedEvent;
+
+    @FindBy(xpath = "//button[contains(text(),'More')]") // also can be created with "." like this //button[contains(.,'More')]
+    public WebElement moreButton;
+
+    @FindBy (xpath = "(//button[@class='icon action-item__menutoggle action-item__menutoggle--default-icon'])[4]")
+    public WebElement menuIcon;
+
+    @FindBy (xpath = "//span[.='Delete']")
+    public WebElement deleteButton;
+
+
 
 
 
